433462bd810461353270cb7be10a78f10a92c743
mdsiyam01325251695016080-maker <mdsiyam01325251695016080@gmail.com>
2026-04-12 01:35:15 +0600

    Update Cr.js

--- scripts/cmds/Cr.js
+++ scripts/cmds/Cr.js
@@ -1,8 +1,8 @@
 module.exports = {
 config: {
 name: "cr",
-version: "2.0",
-author: "〲MAMUNツ࿐ T.T　o.O",
+version: "2.1",
+author: "〲♦ Siyam_YT ♦ツ࿐",
 countDown: 5,
 role: 2,
 shortDescription: "Create group with mention",
@@ -16,29 +16,33 @@ onStart: async function ({ api, event, args }) {
 const mentions = Object.keys(event.mentions);
 
 if (mentions.length == 0) {
-return api.sendMessage("⚠️ Please mention users\nExample:\n-cr Pookie Group @user", event.threadID);
+return api.sendMessage("⚠️ Please mention users\nExample:\n-cr Dream Group @user", event.threadID);
 }
 
 const groupName = args.join(" ").replace(/@\S+/g, "").trim();
 
 if (!groupName) {
-return api.sendMessage("⚠️ Give group name", event.threadID);
+return api.sendMessage("⚠️ Please provide a group name!", event.threadID);
 }
 
 const members = [...mentions, event.senderID];
 
 api.createNewGroup(members, groupName, (err, info) => {
 
-if (err) return api.sendMessage("❌ Group create failed!", event.threadID);
+if (err) return api.sendMessage("❌ Failed to create group!", event.threadID);
 
 api.sendMessage(
-`🎉 GROUP CREATED
+`🎉 𝗚𝗥𝗢𝗨𝗣 𝗖𝗥𝗘𝗔𝗧𝗘𝗗 𝗦𝗨𝗖𝗖𝗘𝗦𝗦𝗙𝗨𝗟𝗟𝗬
 
-👑 Owner: ${event.senderID}
-👥 Members: ${members.length}
-📌 Name: ${groupName}
+👑 Owner: Uday Hasan Siyam
+🌍 Country: Bangladesh
+🎂 Age: 16+
+📌 Group Name: ${groupName}
+👥 Total Members: ${members.length}
 
-♡┋ 𝙋𝙊𝙊𝙆𝙄𝙀 ᥫ᭡🎀🙂`,
+✿•≫───────────────≪•✿
+『 💫 Welcome to my new group! Stay active & enjoy 💙 』
+✿•≫───────────────≪•✿`,
 info.threadID
 );
 
